fbc6bef4aa8dd8dd6a5b30b0af653208ddfdf3cc
spectr-e <devs.josia@gmail.com>
2023-02-16 17:58:14 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 25, 'M', 'black', 'serious', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 15, 'M', 'orange', 'depressed', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 28, 'F', 'green', 'charming', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 35, 'M', 'brown', 'adaptive', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 67, 'F', 'tan', 'altruistic', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 5, 'M', 'pearl', 'optimistic', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 17, 'F', 'blue', 'caring', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 10, 'F', 'red', NULL, true);
